078244911ec693a91a6e618b1088cc23598cc1b0
Alexandre-Monney <alexandre.monney77@gmail.com>
2022-07-20 16:53:44 +0200

    tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Ze m'appelle tony et ze sé po parler anglé lolilol
+        Ze m'appelle tony et ze sé po parler anglé loul
       </header>
     </div>
   );
