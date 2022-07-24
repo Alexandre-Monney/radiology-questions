e2a59f720c058b9c8e4747cc3048a7e7d9b6dc97
Alexandre-Monney <alexandre.monney77@gmail.com>
2022-07-24 16:47:50 +0200

    HomePage cleaned, waiting for Pages.jsx too

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,6 +3,25 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
 
+const links = [
+  // Pages.jsx will be in remplacement after export
+  {
+    id: 1,
+    path: '/irm',
+    name: 'IRM',
+  },
+  {
+    id: 2,
+    path: '/scanner',
+    name: 'Scanner',
+  },
+  {
+    id: 3,
+    path: '/radio',
+    name: 'Radio',
+  },
+];
+
 const Navigation = ({ resetStates }) => {
   useEffect(() => {
     resetStates();
@@ -10,15 +29,11 @@ const Navigation = ({ resetStates }) => {
 
   return (
     <div className='nav'>
-      <Link className='nav-button' to='/irm'>
-        IRM
-      </Link>
-      <Link className='nav-button' to='/scanner'>
-        Scanner
-      </Link>
-      <Link className='nav-button' to='/radio'>
-        Radio
-      </Link>
+      {links.map((link) => (
+        <Link key={link.id} className='nav-button' to={link.path}>
+          {link.name}
+        </Link>
+      ))}
     </div>
   );
 };
