2df17c88a06e68f3a8a0889ff3d97040112c9430
Alexandre-Monney <alexandre.monney77@gmail.com>
2022-07-29 11:00:58 +0200

    bcgcolor changed

---
+++ src/datas/scan.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    id: 1,
+    lang: 'fr',
+    question: 'Comment allez vous ? (scanner)',
+  },
+  {
+    id: 2,
+    lang: 'fr',
+    question: 'Quel age avez vous 2 ? (scanner)',
+  },
+  {
+    id: 3,
+    lang: 'fr',
+    question: 'Comment allez vous 3 ? (scanner)',
+  },
+  {
+    id: 4,
+    lang: 'fr',
+    question: 'Comment allez vous 4 ? (scanner)',
+  },
+  {
+    id: 5,
+    lang: 'en',
+    question: 'How are you ? (scanner)',
+  },
+  {
+    id: 6,
+    lang: 'en',
+    question: 'How are you 2 ? (scanner)',
+  },
+  {
+    id: 7,
+    lang: 'en',
+    question: 'How are you 3 ? (scanner)',
+  },
+  {
+    id: 8,
+    lang: 'en',
+    question: 'How are you 4 ? (scanner)',
+  },
+  {
+    id: 9,
+    lang: 'es',
+    question: 'Como Estas ? (scanner)',
+  },
+  {
+    id: 10,
+    lang: 'es',
+    question: 'Como Estas 2 ? (scanner)',
+  },
+  {
+    id: 11,
+    lang: 'es',
+    question: 'Como Estas 3 ? (scanner)',
+  },
+  {
+    id: 12,
+    lang: 'es',
+    question: 'Como Estas 4 ? (scanner)',
+  },
+];
